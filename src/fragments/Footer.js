478215583f825f24d9a17b73a52a007cdc59c609
djhsingh <djhsingh0001@gmail.com>
2022-11-30 13:26:10 +0530

    react app fresh code

---
+++ src/fragments/Footer.js
@@ -0,0 +1,17 @@
+
+function Footer() {
+  return (
+    <>
+<footer class="text-center text-lg-start bg-danger text-muted fixed-bottom">
+  
+  <div class="text-center p-4" >
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+
+</footer>
+    </>
+  );
+}
+
+export default Footer;
